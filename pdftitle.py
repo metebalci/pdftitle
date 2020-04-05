aad5d633ee0d7df55e4203855bbf84e09905be0d
tommyod <tommy.odland@gmail.com>
2020-04-05 11:52:39 +0200

    sanitize filename

--- pdftitle.py
+++ pdftitle.py
@@ -2,6 +2,7 @@ import sys
 import argparse
 import traceback
 import os
+import string
 from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
 from pdfminer.psparser import literal_name
@@ -541,17 +542,28 @@ def run():
         VERBOSE = args.verbose
         MISSING_CHAR = args.replace_missing_char
         title = get_title_from_file(args.pdf)
+        
+        # If no name was found, return a non-zero exit code
         if title is None:
             return 1
         
+        # If the user wants to change the name of the file
         if args.change_name:
-            new_name = f"{title.replace(' ', '_')}.pdf"
+            
+            # Change the title to a more pleasant file name
+            new_name = title.lower() # Lower case name
+            valid_chars = set(string.ascii_lowercase + string.digits + " ")
+            new_name = "".join(c for c in new_name if c in valid_chars)
+            new_name = new_name.replace(' ', '_') + ".pdf"
+
             os.rename(args.pdf, new_name)
             print(f"Renamed to: {new_name}")
             return 0
         
+        # Else print the title and return 
         print(title)
         return 0
+    
     except Exception as e:
         if VERBOSE:
             traceback.print_exc()
