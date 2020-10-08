b9e90d408c478f1131b8a11b3b704a55f71edc34
Mete Balci <metebalci@gmail.com>
2020-10-08 20:10:13 +0200

    test fix

--- test_max2.sh
+++ test_max2.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 title=$(pdftitle -p paran2010.pdf -a max2 -t)
 if [ $? -eq 0 ]; then
-  if [ ! "$title" = "Settlement Remains from the Bronze and Iron Ages at Horbat Menorim (El-Manara), Lower Galilee" ]; then
+  if [ ! "$title" = "Settlement Remains From The Bronze And Iron Ages At Horbat Menorim (El-Manara), Lower Galilee" ]; then
     exit 1
   fi
   exit 0
