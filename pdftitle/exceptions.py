2798b32e956cc0348d346990eeeaf9eeb50da775
Mete Balci <metebalci@gmail.com>
2024-10-07 15:42:23 +0200

    some renaming

--- pdftitle/exceptions.py
+++ pdftitle/exceptions.py
@@ -1,4 +1,6 @@
+"""pdftitle exceptions"""
 
 class PDFTitleException(Exception):
+    """base class for all pdftitle exceptions"""
     def __init__(self, message):
         super().__init__(message)
